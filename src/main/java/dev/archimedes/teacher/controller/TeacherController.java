e15c0154d1bf77162317819a9701bdcbaa3f367e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-02-25 08:43:05 +0530

    Added new feature API
    teacher can fetch student details for marking attendance
    teacher can then mark the attendance in bulk

--- src/main/java/dev/archimedes/teacher/controller/TeacherController.java
+++ src/main/java/dev/archimedes/teacher/controller/TeacherController.java
@@ -1,4 +1,64 @@
 package dev.archimedes.teacher.controller;
 
+import dev.archimedes.converters.AttendanceConverter;
+import dev.archimedes.dtos.AttendanceDTO;
+import dev.archimedes.entities.Attendance;
+import dev.archimedes.repositories.AttendanceRepository;
+import dev.archimedes.service.contract.EncryptionService;
+import dev.archimedes.teacher.service.TeacherService;
+import dev.archimedes.utils.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/teacher")
+@RequiredArgsConstructor
 public class TeacherController {
+
+    private final AttendanceRepository attendanceRepository;
+
+    private final TeacherService teacherService;
+
+    private final AttendanceConverter attendanceConverter;
+
+    private final EncryptionService hexEncryptionService;
+
+    @GetMapping("/fetch-data")
+    public ResponseEntity<?> fetchStudentToMarkAttendance(
+            @RequestParam("standard") String standard, @RequestParam("section") String section,
+            @RequestParam("subject") String subject, @RequestParam("teacherId") String teacherId
+            ) {
+        List<Attendance> attendances = teacherService.fetchData(standard, section, subject, Integer.parseInt(hexEncryptionService.decrypt(teacherId)));
+        ApiResponse apiResponse = ApiResponse.generateResponse("Student fetched successfully", HttpStatus.OK);
+        apiResponse.setObject(getAttendanceDTOS(attendances, new ArrayList<>()));
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/save-attendance")
+    public ResponseEntity<?> saveAttendanceToStudent(@RequestBody List<AttendanceDTO> attendanceDTOS){
+        return teacherService.saveAllAttendances(getAttendances(attendanceDTOS, new ArrayList<>()));
+    }
+
+    List<AttendanceDTO> getAttendanceDTOS(List<Attendance> attendances, List<AttendanceDTO> attendanceDTOS){
+        for(Attendance attendance: attendances){
+            attendanceDTOS.add(
+                    attendanceConverter.convert(attendance, null)
+            );
+        }
+        return attendanceDTOS;
+    }
+
+    List<Attendance> getAttendances(List<AttendanceDTO> attendanceDTOS, List<Attendance> attendances){
+        for (AttendanceDTO attendanceDTO: attendanceDTOS){
+            attendances.add(
+                    attendanceConverter.reverseConvert(attendanceDTO, null)
+            );
+        }
+        return attendances;
+    }
 }
--- src/main/java/dev/archimedes/teacher/service/TeacherService.java
+++ src/main/java/dev/archimedes/teacher/service/TeacherService.java
@@ -1,6 +1,8 @@
 package dev.archimedes.teacher.service;
 
 import dev.archimedes.entities.Attendance;
+import dev.archimedes.entities.Student;
+import dev.archimedes.enums.AttendanceType;
 import dev.archimedes.repositories.AttendanceRepository;
 import dev.archimedes.repositories.StudentRepository;
 import dev.archimedes.utils.ApiResponse;
@@ -8,6 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +33,25 @@ public class TeacherService {
         }catch (Exception e){
             throw new RuntimeException(e.getLocalizedMessage());
         }
+    }
+
+    public List<Attendance> fetchData(String standard, String section, String subject, int teacherId) {
+        List<Student> students = studentRepository.findStudentsByStandardAndSection(standard, section);
+        List<Attendance> attendances = new ArrayList<>();
+        for (Student student: students){
+            Attendance attendance = new Attendance();
+            attendance.setDate(new Date());
+            attendance.setSubject(subject);
+            attendance.setTeacherId(teacherId);
+            attendance.setAttendanceType(AttendanceType.SUBJECT);
+            attendance.setStudent(student);
+            attendances.add(attendance);
+        }
+        return attendances;
+    }
+
+    public ResponseEntity<?> saveAllAttendances(List<Attendance> attendances) {
+        attendanceRepository.saveAll(attendances);
+        return new ResponseEntity<>(ApiResponse.generateResponse("Successfully Marked", HttpStatus.OK), HttpStatus.OK);
     }
 }
