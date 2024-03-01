826d5540542f4f2ae5f29706098bc8cae23ead8e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-01 12:59:26 +0530

    Added Exception Handling for IllegalArgumentException, AttendanceException and HttpMessageNotReadableException

--- src/main/java/dev/archimedes/advices/GlobalExceptionHandler.java
+++ src/main/java/dev/archimedes/advices/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package dev.archimedes.advices;
 
+import dev.archimedes.teacher.exception.AttendanceException;
 import dev.archimedes.utils.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,5 +36,42 @@ public class GlobalExceptionHandler {
                 .object(errors)
                 .build();
         return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ApiResponse messageNotReadableException(
+            HttpMessageNotReadableException messageNotReadableException,
+            HttpServletRequest httpServletRequest){
+        return ApiResponse.builder()
+                .message("HTTP message not readable")
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .urlPath(httpServletRequest.getRequestURI())
+                .date(new Date())
+                .object(messageNotReadableException.getLocalizedMessage())
+                .build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ApiResponse illegalArgumentException(
+            IllegalArgumentException argumentException, HttpServletRequest servletRequest){
+        return ApiResponse.builder()
+                .message("Illegal Argument Exception")
+                .urlPath(servletRequest.getRequestURI())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .object(argumentException.getLocalizedMessage())
+                .date(new Date())
+                .build();
+    }
+
+    @ExceptionHandler(AttendanceException.class)
+    public ResponseEntity<?> attendanceException(AttendanceException attendanceException, HttpServletRequest servletRequest){
+        return new ResponseEntity<>(ApiResponse.builder()
+                .message(attendanceException.getLocalizedMessage())
+                .urlPath(servletRequest.getRequestURI())
+                .date(new Date())
+                .object(null)
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .build()
+                , HttpStatus.BAD_REQUEST);
     }
 }
