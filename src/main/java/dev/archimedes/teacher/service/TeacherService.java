b3a28b596f711b8edd000615f97f11ed5da6ff63
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-01 13:02:54 +0530

    Update TeacherService.java

--- src/main/java/dev/archimedes/teacher/service/TeacherService.java
+++ src/main/java/dev/archimedes/teacher/service/TeacherService.java
@@ -3,16 +3,19 @@ package dev.archimedes.teacher.service;
 import dev.archimedes.entities.Attendance;
 import dev.archimedes.entities.Student;
 import dev.archimedes.enums.AttendanceType;
+import dev.archimedes.enums.Period;
 import dev.archimedes.repositories.AttendanceRepository;
 import dev.archimedes.repositories.StudentRepository;
+import dev.archimedes.teacher.dto.AttendanceSearchParam;
+import dev.archimedes.teacher.exception.AttendanceException;
 import dev.archimedes.utils.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -35,23 +38,73 @@ public class TeacherService {
         }
     }
 
-    public List<Attendance> fetchData(String standard, String section, String subject, int teacherId) {
+    public List<Attendance> fetchData(String standard, String section, String subject, Period period, int teacherId, LocalDate date) {
+
+        System.out.println(attendanceRepository.markedAttendanceCount(subject, standard, section, period, date));
+        System.out.println(STR."Standard: \{standard} Section: \{section} Subject: \{subject} Period: \{period.name()} Date: \{date}");
+
+        if(attendanceRepository.isAttendanceMarked(subject, standard, section, period, date)){
+            throw new AttendanceException("Attendance Already Marked");
+        }
+
         List<Student> students = studentRepository.findStudentsByStandardAndSection(standard, section);
+
         List<Attendance> attendances = new ArrayList<>();
         for (Student student: students){
             Attendance attendance = new Attendance();
-            attendance.setDate(new Date());
+            attendance.setStandard(standard);
+            attendance.setSection(section);
             attendance.setSubject(subject);
+            attendance.setPeriod(period);
+            attendance.setDate(LocalDate.now());
             attendance.setTeacherId(teacherId);
             attendance.setAttendanceType(AttendanceType.SUBJECT);
             attendance.setStudent(student);
             attendances.add(attendance);
         }
+
         return attendances;
     }
 
-    public ResponseEntity<?> saveAllAttendances(List<Attendance> attendances) {
-        attendanceRepository.saveAll(attendances);
+    public boolean updateAttendance(List<Attendance> attendances){
+        try {
+            for(Attendance attendance: attendances){
+                if(attendanceRepository.existsById(attendance.getId())){
+                    attendanceRepository.save(attendance);
+                }
+            }
+            return true;
+        }catch (Exception e){
+            throw new AttendanceException("Unexpected error during updating attendance");
+        }
+    }
+
+    public ResponseEntity<?> saveAllAttendances(List<Attendance> attendances, AttendanceSearchParam searchParam) {
+
+        System.out.println(STR."Standard: \{searchParam.getStandard()} Section: \{searchParam.getSection()} Subject: \{searchParam.getSubject()} Period: \{searchParam.getPeriod().name()} Date: \{searchParam.getDate()}");
+        System.out.println(attendanceRepository.markedAttendanceCount(
+                searchParam.getSubject(), searchParam.getStandard(), searchParam.getSection(), searchParam.getPeriod(), searchParam.getDate())
+        );
+        System.out.println(attendanceRepository.isAttendanceMarked(
+                searchParam.getSubject(), searchParam.getStandard(), searchParam.getSection(), searchParam.getPeriod(), searchParam.getDate())
+        );
+
+        for (Attendance attendance: attendances){
+
+            if (!attendanceRepository.isAttendanceMarked(searchParam.getSubject(), searchParam.getStandard(),
+                    searchParam.getSection(), searchParam.getPeriod(), searchParam.getDate())
+            ){
+                attendanceRepository.save(attendance);
+            }else {
+                throw new AttendanceException("Attempt to update marked attendance is not allowed please try via update attendance");
+            }
+        }
         return new ResponseEntity<>(ApiResponse.generateResponse("Successfully Marked", HttpStatus.OK), HttpStatus.OK);
+    }
+
+    public List<Attendance> getMarkedAttendance(AttendanceSearchParam searchParam){
+        return attendanceRepository.findAttendanceToUpdateForAPeriod(
+                searchParam.getSubject(), searchParam.getStandard(),
+                searchParam.getPeriod(), searchParam.getDate(), searchParam.getSection());
     }
 }
